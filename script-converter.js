47382da360ef2859eb2ce4083113d86ef566e8ec
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-08 19:46:51 +0800

    Create script-converter.js

---
+++ script-converter.js
@@ -0,0 +1,11 @@
+// 导出函数供外部使用
+module.exports = {
+  parseScript,
+  convertToSurge,
+  convertToLoon,
+  processScript,
+  extractScriptContent,
+  processFromGitHub,
+  processSingleFile,
+  batchProcessDirectory
+};
