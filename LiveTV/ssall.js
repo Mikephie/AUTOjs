51f67acd67c72d2ad888c753461e10a371ca8dae
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-08-28 10:36:03 +0800

    o

---
+++ LiveTV/ssall.js
@@ -0,0 +1,34 @@
+{
+  "title": "🅜🅘🅚🅔®️🐲 '𝓼 𝓦𝓲𝓭𝓰𝓮𝓽𝓼",
+  "description": "瑟瑟合集",
+  "icon": "https://widgets-xd.vercel.app/icon/icon.PNG",
+  "widgets": [
+    {
+      "id": "missav",
+      "title": "MissAV",
+      "description": "获取 MissAV 推荐",
+      "requiredVersion": "0.0.2",
+      "version: "2.0.0",
+      "author": "Mikephie",
+      "url": "https://raw.githubusercontent.com/Mikephie/AUTOjs/refs/heads/main/LiveTV/missav.js"
+    },
+    {
+      "id": "Pornhub",
+      "title": "Pornhub",
+      "description": "在线观看Pornhub",
+      "requiredVersion": "0.0.1",
+      "version: "2.0.0",
+      "author": "Mikephie",
+      "url": "https://raw.githubusercontent.com/Mikephie/AUTOjs/refs/heads/main/LiveTV/ph.js"
+    },
+    {
+      "id": "xvideos",
+      "title": "XVideos",
+      "description": "在线观看XVideos",
+      "requiredVersion": "0.0.1",
+      "version: "2.0.0",
+      "author": "Mikephie",
+      "url": "https://raw.githubusercontent.com/Mikephie/AUTOjs/refs/heads/main/LiveTV/xv.js"
+    }
+  ]
+}
