3edff79d006a1138f8dd600893ff79c0d991a4f1
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-09-02 13:21:56 +0800

    Update build-collection-surge.js

--- scripts/build-collection-surge.js
+++ scripts/build-collection-surge.js
@@ -116,7 +116,8 @@ function build(){
     '#!name=mcollection 🔐APP',
     '#!desc=自动合并生成的合集模块 (Surge)',
     '#!author=🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ',
-    '#!category=🔐APP'
+    '#!category=🔐APP',
+    '#!icon=https://raw.githubusercontent.com/Mikephie/icons/main/loon/heji.gif'
   ].join('\n');
 
   let out = header + '\n';
