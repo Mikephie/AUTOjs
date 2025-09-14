e7c4c70ab551f68616c252f5f883193fe9b7a1e7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-09-14 16:40:18 +0800

    o

--- Tempermonkey/githubplus.user.js
+++ Tempermonkey/githubplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub 助手增强版完善版
 // @namespace    https://github.com/
-// @version      8.0
+// @version      8.1
 // @author       Mr.Eric
 // @license      MIT
 // @description  修复 GitHub 下载 ZIP / Raw 链接，自动获取所有分支选择下载，添加文件编辑和保存功能。Gist面板显示私库和公库，增加复制Git链接功能（兼容旧浏览器剪贴板）。添加Sync Fork按钮，修复Mac Safari背景适配问题。支持面板拖拽和调整大小，特别添加iOS设备支持。新增Actions工作流及编辑功能。
