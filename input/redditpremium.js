437c33effd8ebde1249ddb08a3b3e9ea31b35d9c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-09-23 09:16:58 +0800

    o

--- input/redditpremium.js
+++ input/redditpremium.js
@@ -6,7 +6,7 @@
 #!icon=https://raw.githubusercontent.com/Mikephie/icons/main/icon/reddit.png
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 [rewrite_local]
-^https?:\/\/gql-fed\.reddit\.com\/$ url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/redditpremium.js
+^https?:\/\/gql(-fed)?\.reddit\.com\/.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/redditpremium.js
 
 [MITM]
 hostname = gql-fed.reddit.com
